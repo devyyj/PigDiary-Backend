75a5b34c856c087ea6d3008cee2188eabec7883d
devyyj <ryong0906@naver.com>
2023-08-28 20:30:02 +0900

    first commit

---
+++ src/main/java/com/example/pigdiary/PigDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.pigdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PigDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PigDiaryApplication.class, args);
+    }
+
+}
